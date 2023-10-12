8894d3864af9410386b132b27431603f80c38576
Pulsar7 <pythonpulsar@gmail.com>
2023-10-12 22:39:56 +0200

    modified:   startup.sh

--- startup.sh
+++ startup.sh
@@ -4,3 +4,8 @@ python3 -m venv venv
 source venv/bin/activate
 pip3 install -r requirements.txt -v
 python3 magic.py
+if [ -n "$VIRTUAL_ENV" ]; then
+    echo "Virtual-environment: $VIRTUAL_ENV"
+    exit
+fi
+echo "No virtual-environment"
